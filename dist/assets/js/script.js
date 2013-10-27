269a0fdf1cc75b770fd0fe9e34686503a58fc959
Pratama Hasriyan S <pratama@inkreastika.com>
2013-10-27 09:07:11 +0700

    remove jscrollpane, fixed navbar expanded styles

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,35 +1,44 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+    'use strict';
     /**
      * Resize Jumbotron when window resizes
      */
     var $wh = jQuery(window).outerHeight();
     jQuery('.jumbotron').css('height', $wh);
-    jQuery(window).on('resize', function(){
+    jQuery(window).on('resize', function () {
         var $wh = jQuery(window).outerHeight();
-        jQuery('.jumbotron').css('height', $wh)
+        jQuery('.jumbotron').css('height', $wh);
     });
-    
+
     /**
      * Toggle Nav
      */
-    jQuery('.toggle.button').on('click', function(e) {
+    jQuery('.toggle.button').on('click', function (e) {
         e.preventDefault();
-        if(jQuery('.navbar').hasClass('hiden')) {
+        if (jQuery('.navbar').hasClass('hiden')) {
             jQuery('.navbar').animate({
                 'right': '0%'
             });
             jQuery('#right').animate({
                 'margin-right': '15%'
             });
+            jQuery('body').css({
+                'height': jQuery('.navbar').outerHeight(),
+                'overflow': 'hidden'
+            })
             jQuery('.navbar').addClass('shown');
             jQuery('.navbar').removeClass('hiden');
-        } else if(jQuery('.navbar').hasClass('shown')) {
+        } else if (jQuery('.navbar').hasClass('shown')) {
             jQuery('.navbar').animate({
                 'right': '-15%'
             });
             jQuery('#right').animate({
                 'margin-right': '0%'
             });
+            jQuery('body').css({
+                'height': 'auto',
+                'overflow': 'auto'
+            })
             jQuery('.navbar').addClass('hiden');
             jQuery('.navbar').removeClass('shown');
         } else {
@@ -39,13 +48,36 @@ jQuery(document).ready(function(){
             jQuery('#right').animate({
                 'margin-right': '15%'
             });
+            jQuery('body').css({
+                'height': jQuery('.navbar').outerHeight(),
+                'overflow': 'hidden'
+            })
             jQuery('.navbar').addClass('shown');
             jQuery('.navbar').removeClass('hiden');
+        }
+    });
+
+    jQuery('#left, #right').on('click', function (e) {
+        if (jQuery('.navbar').hasClass('hiden')) {
+            
+        } else if (jQuery('.navbar').hasClass('shown')) {
+            jQuery('.navbar').animate({
+                'right': '-15%'
+            });
+            jQuery('#right').animate({
+                'margin-right': '0%'
+            });
+            jQuery('body').css({
+                'height': 'auto',
+                'overflow': 'auto'
+            })
+            jQuery('.navbar').addClass('hiden');
+            jQuery('.navbar').removeClass('shown');
         }
     })
 
     /**
      * Scrollpane
      */
-    jQuery('.scroll-pane').jScrollPane();
+    // jQuery('.scroll-pane').jScrollPane();
 });
