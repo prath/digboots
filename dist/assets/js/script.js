2a2c3da4c2aa06a5e163a979074d26919385f3a9
Pratama Hasriyan S <pratama@inkreastika.com>
2014-02-03 13:07:40 +0700

    lupa beutify

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -3,114 +3,113 @@
  * doc ready
  * ==============================================================================
  */
-$(document).ready(function () {
-    'use strict';
+$(document).ready(function() {
+	'use strict';
 
-    var $wh = $(window).outerHeight();
+	var $wh = $(window).outerHeight();
 
-    /**
-     * Resize Jumbotron when window resizes
-     */
-    $('.jumbotron').css('height', $wh);
-    $(window).on('resize', function () {
-        var $wh = $(window).outerHeight();
-        $('.jumbotron').css('height', $wh);
-    });
+	/**
+	 * Resize Jumbotron when window resizes
+	 */
+	$('.jumbotron').css('height', $wh);
+	$(window).on('resize', function() {
+		var $wh = $(window).outerHeight();
+		$('.jumbotron').css('height', $wh);
+	});
 
-    /**
-     * Map Container Resize
-     */
-    $('#map-canvas').css('height', $wh);
-    $(window).on('resize', function () {
-        var $wh = $(window).outerHeight();
-        $('#map-canvas').css('height', $wh);
-    });
+	/**
+	 * Map Container Resize
+	 */
+	$('#map-canvas').css('height', $wh);
+	$(window).on('resize', function() {
+		var $wh = $(window).outerHeight();
+		$('#map-canvas').css('height', $wh);
+	});
 
-    /**
-     * Toggle Collapsible Content.
-     * searchbox, navbar, cartbar, and sidebar expand-collapse
-     */
-    $('.top.nav .toggle').on('click', function(e){
-        e.preventDefault();
-        if(!$('body').hasClass('toggle' + $(this).attr('data-content'))) {
-            $('body').removeAttr('class');
-        }
-        $('body').toggleClass('toggle' + $(this).attr('data-content'));
-        $('.searchinput').focus();
-    })
+	/**
+	 * Toggle Collapsible Content.
+	 * searchbox, navbar, cartbar, and sidebar expand-collapse
+	 */
+	$('.top.nav .toggle').on('click', function(e) {
+		e.preventDefault();
+		if (!$('body').hasClass('toggle' + $(this).attr('data-content'))) {
+			$('body').removeAttr('class');
+		}
+		$('body').toggleClass('toggle' + $(this).attr('data-content'));
+		$('.searchinput').focus();
+	})
 
-    /**
-     * collapse all collapsible content
-     */
-    $('#left, #right').children(':not(".sidebar")').on('click', function (e) {
-        $('body').removeAttr('class');
-    });
+	/**
+	 * collapse all collapsible content
+	 */
+	$('#left, #right').children(':not(".sidebar")').on('click', function(e) {
+		$('body').removeAttr('class');
+	});
 
-    
-    /**
-     * Toggle Tooltip
-     */
-    $('.sticky.nav').tooltip({
-        selector: "[data-toggle=tooltip]",
-        container: "body"
-    });
 
-    /**
-     * FitVids
-     */
-    $(".fitvids").fitVids();
+	/**
+	 * Toggle Tooltip
+	 */
+	$('.sticky.nav').tooltip({
+		selector: "[data-toggle=tooltip]",
+		container: "body"
+	});
 
-    /**
-     * Appear
-     */
-    $('.appear').appear();
+	/**
+	 * FitVids
+	 */
+	$(".fitvids").fitVids();
 
-    $(document.body).on('appear', '.appear', function(e, $affected) {
-        $affected.each(function(i) {
-            var $current = $(this),
-                $delay = i*50;
-            $current.delay($delay).queue(function(){
-                $current.addClass('animate');
-            })
-        })
-    });
+	/**
+	 * Appear
+	 */
+	$('.appear').appear();
 
-    /**
-     * FLexislider
-     */
-    $('.flexslider').flexslider({
-        animation: "slide",
-        animationLoop: false,
-        itemWidth: 88,
-        itemMargin: 0,
-        pausePlay: false,
-        selector: ".client-slides > li",
-        directionNav: false,
-        start: function(slider){
-        }
-    });
+	$(document.body).on('appear', '.appear', function(e, $affected) {
+		$affected.each(function(i) {
+			var $current = $(this),
+				$delay = i * 50;
+			$current.delay($delay).queue(function() {
+				$current.addClass('animate');
+			})
+		})
+	});
 
-    /**
-     * Google Map
-     */
-    var mapOptions = {
-        zoom: 15,
-        scrollwheel: false,
-        center: new google.maps.LatLng(40.64432, -74.01107),
-        // zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.LEFT_TOP
-        },
-        // scaleControl: false,
-        scaleControlOptions: {
-            position: google.maps.ControlPosition.BOTTOM_LEFT
-        },
-        streetViewControl: false,
-        panControl: false,
-        mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    
+	/**
+	 * FLexislider
+	 */
+	$('.flexslider').flexslider({
+		animation: "slide",
+		animationLoop: false,
+		itemWidth: 88,
+		itemMargin: 0,
+		pausePlay: false,
+		selector: ".client-slides > li",
+		directionNav: false,
+		start: function(slider) {}
+	});
+
+	/**
+	 * Google Map
+	 */
+	var mapOptions = {
+		zoom: 15,
+		scrollwheel: false,
+		center: new google.maps.LatLng(40.64432, -74.01107),
+		// zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL,
+			position: google.maps.ControlPosition.LEFT_TOP
+		},
+		// scaleControl: false,
+		scaleControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_LEFT
+		},
+		streetViewControl: false,
+		panControl: false,
+		mapTypeControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
 });
